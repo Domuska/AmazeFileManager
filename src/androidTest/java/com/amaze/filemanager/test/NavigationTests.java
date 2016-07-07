368d87d6d7b37ccb0ac577c5c8d771567e7d660a
Tomi Lamsa <lamsatom@gmail.com>
2016-07-07 10:39:51 +0300

    added toString to drawerItem so the drawer list can be scrolled to correct element, finished test to swipe between the two views

--- src/androidTest/java/com/amaze/filemanager/test/NavigationTests.java
+++ src/androidTest/java/com/amaze/filemanager/test/NavigationTests.java
@@ -1,12 +1,9 @@
 package com.amaze.filemanager.test;
 
-import android.support.test.espresso.contrib.DrawerActions;
-import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
-import com.amaze.filemanager.*;
 import com.amaze.filemanager.activities.MainActivity;
 import com.amaze.filemanager.R;
 
@@ -18,25 +15,23 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.action.ViewActions.swipeRight;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withChild;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.anything;
-import static org.hamcrest.CoreMatchers.endsWith;
-import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.hasToString;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class NavigationTests {
 
-    private String storageText, recentFilesText;
+    private String storageText, recentFilesText, videosText;
 
     @Rule
     public ActivityTestRule<MainActivity> myActivityRule =
@@ -48,41 +43,59 @@ public class NavigationTests {
                 .getResources().getString(com.amaze.filemanager.R.string.storage);
         recentFilesText = myActivityRule.getActivity().getApplicationContext()
                 .getResources().getString(R.string.recent);
+        videosText = myActivityRule.getActivity().getApplicationContext()
+                .getResources().getString(R.string.videos);
     }
 
     @Test
     public void testSwipingBetweenTwoFolders(){
 
-        //navigate one view into the storage
+        //make sure we're on the leftmost screen
+        swipeScreenRight();
+
+        //navigate one view into the images
         Utils.openDrawer();
-        onView(withText(storageText)).perform(click());
+        onData(hasToString(videosText))
+                .inAdapterView(withId(R.id.menu_drawer))
+                .perform(click());
 
         //swipe into the other list
-        onView(allOf(withId(com.amaze.filemanager.R.id.listView), isDisplayed()))
-                .perform(swipeRight());
+        swipeScreenLeft();
 
-        //open recent files
+
         Utils.openDrawer();
-//        onData(allOf(withId(R.id.menu_drawer), withText(recentFilesText))).perform(scrollTo());
-//        onData(withClassName(endsWith("DrawerAdapter"))).perform(click());
-//        onData(allOf(is(instanceOf(String.class)), is(recentFilesText)))
+
+        //open 'Recent Files'
+        onData(hasToString(recentFilesText))
+                .inAdapterView(withId(R.id.menu_drawer))
+                .perform(click());
+
+//        onData(hasProperty("Title", equalTo(recentFilesText)))
+//                .inAdapterView(withId(R.id.menu_drawer))
+//                .perform(click());
+        //löytää useampia resultteja, pitäs tarkentaa
+//        onData(allOf(is(instanceOf(EntryItem.class)))).perform(click());
 
         //works, but is a scheisse way to do it
 //        onData(anything()).inAdapterView(withId(R.id.menu_drawer))
 //                .atPosition(11).perform(click());
 
-
-        onData().perform(click());
-
-//        onData(anything()).inAdapterView(withId(R.id.menu_drawer))
-//                .onChildView(withText(recentFilesText)).perform(click());
-
-//        onData(withClassName(endsWith("DrawerAdapter")))
-//                .inAdapterView(withId(R.id.menu_drawer))
-//                .perform(click());
+        swipeScreenRight();
+        onView(withId(R.id.fullpath)).check(matches(allOf(isDisplayed(), withText(videosText))));
+        swipeScreenLeft();
+        onView(withId(R.id.fullpath)).check(matches(allOf(isDisplayed(), withText(recentFilesText))));
 
 
-//                .onChildView(withText(recentFilesText)).perform(click());
+    }
 
+    //0 for left, 1 for right
+    private void swipeScreenLeft(){
+        onView(allOf(withId(R.id.pager), isDisplayed()))
+                .perform(swipeLeft());
+    }
+
+    private void swipeScreenRight(){
+        onView(allOf(withId(R.id.pager), isDisplayed()))
+                .perform(swipeRight());
     }
 }
