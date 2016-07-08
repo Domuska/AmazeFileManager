31b4830a703fcdf5c87c07657304f45d4b82c647
Tomi Lamsa <lamsatom@gmail.com>
2016-07-08 09:33:53 +0300

    something was left out from last commit

--- src/androidTest/java/com/amaze/filemanager/test/BookMarkTests.java
+++ src/androidTest/java/com/amaze/filemanager/test/BookMarkTests.java
@@ -1,17 +1,23 @@
 package com.amaze.filemanager.test;
 
+import android.preference.PreferenceManager;
 import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 
 import com.amaze.filemanager.*;
 import com.amaze.filemanager.R;
 import com.amaze.filemanager.activities.MainActivity;
 import com.amaze.filemanager.ui.drawer.EntryItem;
 
+import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,6 +25,8 @@ import org.junit.Test;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -27,6 +35,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.hasToString;
 
 public class BookMarkTests {
@@ -44,6 +53,7 @@ public class BookMarkTests {
                 myActivityRule.getActivity().getApplicationContext().getString(R.string.addtobook);
         generalTestFolderName = TestDataSource.generalTestFolderName;
     }
+
 
     //it is presumed that at this point there is .../Testing folder
     @Test
@@ -63,8 +73,21 @@ public class BookMarkTests {
         onData(allOf(hasToString(generalTestFolderName), is(instanceOf(EntryItem.class))))
                 .inAdapterView(withId(com.amaze.filemanager.R.id.menu_drawer))
                 .check(matches(isDisplayed()));
+
+        //remove the bookmark
+
+        onData(allOf(hasToString(generalTestFolderName), is(instanceOf(EntryItem.class))))
+                .inAdapterView(withId(R.id.menu_drawer))
+                .perform(longClick());
+        onView(withId(R.id.buttonDefaultNegative)).perform(click());
+
+        //assert it is no longer visible, use custom withAdaptedData
+        onView(withId(R.id.menu_drawer))
+                .check(matches(not(withAdaptedData(hasToString(generalTestFolderName)))));
+
     }
 
+    //for clicking the overflow button in the main fragment rows
     private static class clickOverflowAction{
 
         public static ViewAction clickOverflowButton(final int id){
@@ -86,5 +109,33 @@ public class BookMarkTests {
                 }
             };
         }
+    }
+
+    //for checking data is not in the listview adapter
+    //https://google.github.io/android-testing-support-library/docs/espresso/advanced/index.html
+    private static Matcher<View> withAdaptedData(final Matcher<Object> dataMatcher) {
+        return new TypeSafeMatcher<View>() {
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with class name: ");
+                dataMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view instanceof AdapterView)) {
+                    return false;
+                }
+                @SuppressWarnings("rawtypes")
+                Adapter adapter = ((AdapterView) view).getAdapter();
+                for (int i = 0; i < adapter.getCount(); i++) {
+                    if (dataMatcher.matches(adapter.getItem(i))) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        };
     }
 }
