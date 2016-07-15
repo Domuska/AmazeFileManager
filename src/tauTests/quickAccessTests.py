47dfd2a01d516396d8e7417b72f30efc59c3dc64
Tomi Lamsa <lamsatom@gmail.com>
2016-07-15 15:19:46 +0300

    added quickaccesstest, uses same fiddly/flaky way of adding a new file as fileandfoldermanipulation tests do

---
+++ src/tauTests/quickAccessTests.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from Utilities import TestDataSource, Utils
+
+class QuickAccessFileTest(UITestCase):
+
+    def setUp(self):
+        
+        global fileName, quickAccessText, amazeTestingFolder
+        
+        fileName = TestDataSource.movieFileName
+        quickAccessText = "Quick Access"
+        amazeTestingFolder = TestDataSource.amazeTestFolderName
+        
+        Utils.navigateToTestFolder()
+        Utils.createFolderWithName(amazeTestingFolder)
+        tap.text(amazeTestingFolder)
+        
+    #def tearDown(self):
+        #do things        
+
+    @testCaseInfo('<Quick access text>', deviceCount=1)
+    def testOpenFileCheckRecents(self):
+        
+        self.createFileWithName(fileName)
+        
+        #open the file multiple times to add it to quick access
+        self.openFileNavigateBack()
+        self.openFileNavigateBack()
+        self.openFileNavigateBack()
+        self.openFileNavigateBack()
+        
+        #go to quick access
+        Utils.openDrawer()
+        tap.text(quickAccessText)
+        
+        #assert file name is visible here
+        assert exists.text(fileName), \
+        "file " + fileName + " should be visible"
+     
+    def openFileNavigateBack(self):
+         tap.text(fileName)
+         input.key.back()
+         
+    def createFileWithName(self, name):
+        tap.instanceOf('android.widget.ImageView', index=2)
+        tap.resourceId("com.amaze.filemanager:id/menu_item1")
+        input.text(name)
+        tap.resourceId("com.amaze.filemanager:id/buttonDefaultPositive")
+       
