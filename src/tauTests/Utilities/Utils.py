284a965cd85f1d9f0f417cb43131627d9a374069
Tomi Lamsa <lamsatom@gmail.com>
2016-07-15 12:51:46 +0300

    create new folder and delete it test works, it is not optimal implementation though

--- src/tauTests/Utilities/Utils.py
+++ src/tauTests/Utilities/Utils.py
@@ -20,3 +20,4 @@ def openDrawer():
     
 def openFabMenu():
     tap.instanceOf('android.widget.ImageView', area='com.amaze.filemanager:id/menu')
+	
--- src/tauTests/amazeTestSet.py
+++ src/tauTests/amazeTestSet.py
@@ -4,25 +4,46 @@ from Utilities import TestDataSource, Utils
 class FileAndFolderManipulationTests(UITestCase):
     
     def setUp(self):
-        global newFolderName
+        global folderName1
         global testingFolderName
+        global amazeTestingFolder
         
         testingFolderName = TestDataSource.generalTestFolderName
-        newFolderName = TestDataSource.folderName1
+        folderName1 = TestDataSource.folderName1
+        amazeTestingFolder = TestDataSource.amazeTestFolderName
         launch.activity('com.amaze.filemanager', '.activities.MainActivity')
         
+        Utils.navigateToTestFolder()
+        Utils.createFolderWithName(amazeTestingFolder)
+        tap.text(amazeTestingFolder)
         
     #def tearDown(self):
         #packages.clearData('com.amaze.filemanager')
+        #remove the folder
+        
         
         
     @testCaseInfo('<Add a new folder>', deviceCount=1)
     def testCreateNewFolderAndDeleteIt(self):
-        log('we should print stuff')
-        #tap.text(testingFolderName, direction='vertical')
-        #Utils.openDrawer()
-        Utils.navigateToTestFolder()
-        Utils.createFolderWithName(newFolderName)
-        tap.text(newFolderName)
+      
+        #Utils.createFolderWithName(folderName1)
+        #for some reason the way it is done in utils does not work!
+        tap.instanceOf('android.widget.ImageView', index=2)
+        tap.resourceId("com.amaze.filemanager:id/menu_item")
+        input.text(folderName1)
+        tap.resourceId("com.amaze.filemanager:id/buttonDefaultPositive")
+        
+        #assert it is visible
+        exists.text(folderName1)
+        
+        #delete the file
+        tap.long.text(folderName1)
+        tap.resourceId('com.amaze.filemanager:id/delete')
+        tap.resourceId('com.amaze.filemanager:id/buttonDefaultPositive')
+        
+        #assert it is gone
+        exists.no.text(folderName1)
+        
+        
         
         
