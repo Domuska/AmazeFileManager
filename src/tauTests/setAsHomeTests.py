05d604023d7b5e80a08a2abcfa735e6a36f74c27
Tomi Lamsa <lamsatom@gmail.com>
2016-07-18 09:37:53 +0300

    added setAsHomeTest, teardown will now clear all app's data and setup will handle giving permissions if app asks for them at startup. This handling is to be added to other tests aswell

---
+++ src/tauTests/setAsHomeTests.py
@@ -0,0 +1,57 @@
+# -*-acoding: utf-8 -*-
+from Utilities import TestDataSource, Utils
+
+class SetHomeTest(UITestCase):
+    
+    def setUp(self):
+        
+        global generalTestFolderName, setAsHomeText, quickAccessText
+        
+        generalTestFolderName = TestDataSource.generalTestFolderName
+        setAsHomeText = "Set As Home"
+        quickAccessText = "Quick Access"
+        
+        reactor.add("Permission Reactor", self.allowPermissions, text="Allow")
+        launch.activity('com.amaze.filemanager', '.activities.MainActivity', verify=False)
+        #omatekemä launchi
+        #tap.description("Apps")
+        #tap.text("Amaze")
+        
+        Utils.swipeToRightScreen()
+        Utils.navigateToTestFolder()
+        
+    def tearDown(self):
+        input.key.home()
+        packages.clearData('com.amaze.filemanager')
+        
+        
+    def allowPermissions(self):
+        #tap.resourceId("com.android.packageinstaller:id/permission_allow_button")
+        tap.text("Allow")
+
+    @testCaseInfo('<Test setting folder as home>', deviceCount=1)
+    def testSetHome(self):
+        
+        log('Step1: Insert test step description')
+        
+        #set this folder as home
+        Utils.swipeDownInPathBar()
+        Utils.openOverFlowMenu()
+        tap.text(setAsHomeText)
+        tap.resourceId("com.amaze.filemanager:id/buttonDefaultPositive")
+        
+        #navigate to somewhere else
+        Utils.openDrawer()
+        tap.text(quickAccessText)
+        
+        #go back home
+        tap.resourceId("com.amaze.filemanager:id/home")
+        
+        #assert we're at the .../Testing folder
+        pathBar = get.item.resourceId("com.amaze.filemanager:id/fullpath")
+        
+        if generalTestFolderName not in pathBar.Text:
+            fail("folder " + generalTestingFolder + " not in path bar")
+        else:
+            log("Test passed")
+        
