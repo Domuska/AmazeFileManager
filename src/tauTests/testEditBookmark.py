b3cbb5f20215b2385fbdaf68dd75fc4cb648df46
Tomi Lamsa <lamsatom@gmail.com>
2016-08-17 15:29:41 +0300

    setup in edit bookmark test now clicks storage in nav drawer, removed clearData from this class

--- src/tauTests/testEditBookmark.py
+++ src/tauTests/testEditBookmark.py
@@ -4,21 +4,23 @@ from Utilities import TestDataSource, Utils
 class EditBookmarkTests(UITestCase):
 
     def setUp(self):
-        global newTestFolderName, generalTestFolderName
+        global newTestFolderName, generalTestFolderName, storageText
         
         newTestFolderName = TestDataSource.newTestFolderName
         generalTestFolderName = TestDataSource.generalTestFolderName
+        storageText = "Storage"
         
         reactor.add("Permission Reactor", self.allowPermissions, text="Allow")
         #launch app with verify=False so when app launches it won't fail if
         #the app asks for permissions
         launch.activity('com.amaze.filemanager', '.activities.MainActivity', verify=False)
+        Utils.openDrawer()
+        tap.text(storageText)
         
     def tearDown(self):
         tap.long.text(newTestFolderName)
         tap.resourceId("com.amaze.filemanager:id/buttonDefaultNegative")
-        packages.clearData('com.amaze.filemanager')
-        
+        #packages.clearData('com.amaze.filemanager')
         
     def allowPermissions(self):
         tap.resourceId("com.android.packageinstaller:id/permission_allow_button")
